98bf9bd01966eb507e5c96405c0a311950d05910
Evan Koppers <evan.koppers@mymail.champlain.edu>
2021-05-02 20:56:11 -0400

    change project to no longer use URP using shader scripts

---
+++ Accesibilty Shader/Assets/postProcessingCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class postProcessingCamera : MonoBehaviour
+{
+    public Material simulation;
+    public Material fix;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, simulation);
+    }
+}
